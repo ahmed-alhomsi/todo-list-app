444c7b8845fbb3d4934aae73c0bd373916e51342
ahmed-alhomsi <ahmed.alhomsi.official@gmail.com>
2021-12-19 11:29:37 +0200

    changed up the look and made it a bit bitter.

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ function showAllTasks() {
         tasks.forEach(task=>{
             taskList.innerHTML += 
             `
-            <li class="list-group-item bg-secondary text-light">${task}<a href="# " class="mx-2 delete-item text-danger">X</a><a href="#" class="edit-item">Edit</a></li>
+            <div class="d-flex my-2">
+                <li class="list-group-item bg-secondary text-light w-75">${task}</li>
+                <div>
+                    <button type="button" class="btn btn-primary btn primary-btn delete-item p-2 px-3 mx-1">Delete</button>
+                    <button type="button" class="btn btn-danger edit-item p-2 px-4">Edit</button>
+                </div>
+            </div>
             `;
         })
     }
@@ -27,53 +33,57 @@ function showAllTasks() {
 function addTask() {
     const task = taskInput.value;
     if(emptyTask.test(task)) {
-        error.innerHTML = '<p class=".h3">Error: please Enter a task!</p>';
+        error.innerHTML = '<p class="h3">Error: please Enter a task!</p>';
         setTimeout(() => {
             error.innerHTML = '';
-        }, 2000);
+        }, 2500);
     } else if(!tasks.includes(task)) {
         tasks.push(task)
         taskList.innerHTML += 
         `
-        <li class="list-group-item bg-secondary text-light">${task}<a href="# " class="mx-2 delete-item text-danger">X</a><a href="#" class="edit-item">Edit</a></li>
+        <div class="d-flex my-2">
+            <li class="list-group-item bg-secondary text-light w-75">${task}</li>
+                <div class="">
+                    <button type="button" class="btn btn-primary btn primary-btn delete-item p-2 px-3 mx-1">Delete</button>
+                    <button type="button" class="btn btn-danger edit-item p-2 px-4">Edit</button>
+                </div>
+        </div>
         `;
         localStorage.setItem('tasks', JSON.stringify(tasks));
     } else {
-        error.innerHTML = '<p class=".h3">Error: task already exists!</p>';
+        error.innerHTML = '<p class="h3">Error: task already exists!</p>';
         setTimeout(() => {
             error.innerHTML = '';
-        }, 2000);
+        }, 2500);
     }
     taskInput.value = '';
 }
 
 function deleteTask(e) {
     tasks.forEach((task,i) => {
-        if(task == e.target.parentElement.innerText.slice(0,-5)) {
+        if(task == e.target.parentElement.parentElement.innerText.slice(0,-12)) {
             tasks.splice(i,1);
         }
     });
-    e.target.parentElement.remove();
+    e.target.parentElement.parentElement.remove();
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function editTask(e) {
-    text = e.target.parentElement.innerText;
+    text = e.target.parentElement.parentElement.innerText.slice(0,-12);
         const textArr = [];
         for(let i = 0; i < text.length; i++) {
             textArr.push(text[i]);
         }
-        for(let i = 0; i < 5; i++) {
-            textArr.pop()
-        }
         text = textArr.join('');
+        console.log(text)
         taskInput.value = text;
         tasks.forEach((task,i) => {
-            if(task == e.target.parentElement.innerText.slice(0,-5)) {
+            if(task == e.target.parentElement.parentElement.innerText.slice(0,-12)) {
                 tasks.splice(i,1);
             }
         });
-        e.target.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
         localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
@@ -94,7 +104,13 @@ function searchTask(e) {
                 if(!searchedTasks.includes(task)) {
                     searchedTasks.push(task);
                     taskList.innerHTML += `
-                    <li class="list-group-item bg-secondary text-light">${task}<a href="# " class="mx-2 delete-item text-danger">X</a><a href="#" class="edit-item">Edit</a></li>
+                    <div class="d-flex my-2">
+                        <li class="list-group-item bg-secondary text-light w-75">${task}</li>
+                        <div class="">
+                            <button type="button" class="btn btn-primary btn primary-btn delete-item p-2 px-3 mx-1">Delete</button>
+                            <button type="button" class="btn btn-danger edit-item p-2 px-4">Edit</button>
+                        </div>
+                    </div>
                     `;
                 }
             }
@@ -102,7 +118,13 @@ function searchTask(e) {
             taskList.innerHTML = '';
             tasks.forEach(task=>{
                 taskList.innerHTML += `
-                <li class="list-group-item bg-secondary text-light">${task}<a href="# " class="mx-2 delete-item text-danger">X</a><a href="#" class="edit-item">Edit</a></li>
+                <div class="d-flex my-2">
+                    <li class="list-group-item bg-secondary text-light w-75">${task}</li>
+                    <div class="">
+                        <button type="button" class="btn btn-primary btn primary-btn delete-item p-2 px-3 mx-1">Delete</button>
+                        <button type="button" class="btn btn-danger edit-item p-2 px-4">Edit</button>
+                    </div>
+                </div>
                 `;
             });
         }
